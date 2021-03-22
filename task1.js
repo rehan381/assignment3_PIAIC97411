044cbf01aeb7e611dad8ddc2e01253ad42014405
Rehan Ashraf <66112606+rehan381@users.noreply.github.com>
2021-03-22 16:51:27 +0500

    cmint

--- task1.js
+++ task1.js
@@ -1,5 +1,7 @@
 var edu = ["SSC", "HSC", "bcs", "BS", "BCOM", "PHD"];
 alert("Qualification");
+
 document.write("<br>");
 alert(edu[1] , "\n" , edu[2] );
 document.write(edu[1] , "<br>" , edu[2]);
+
